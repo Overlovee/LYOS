1745e85ea56b39648ffdcf480a5fb3947df83a50
I'mFoodboy <vnn159357@gmail.com>
2024-05-26 15:51:28 +0700

    update some feature -,-

--- app/src/main/java/com/example/lyos/Login.java
+++ app/src/main/java/com/example/lyos/Login.java
@@ -34,11 +34,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
 
 public class Login extends AppCompatActivity {
-
-    private SignInClient oneTapClient;
-    private BeginSignInRequest signUpRequest;
-    private static final int REQ_ONE_TAP = 2;
-    private boolean showOneTapUI = true;
     private ActivityLoginBinding activityLoginBinding;
     private GoogleSignInOptions googleSignInOptions;
     private GoogleSignInClient googleSignInClient;
@@ -48,70 +43,12 @@ public class Login extends AppCompatActivity {
         activityLoginBinding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(activityLoginBinding.getRoot());
 
-//        oneTapClient = Identity.getSignInClient(this);
-//        signUpRequest = BeginSignInRequest.builder()
-//                .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
-//                        .setSupported(true)
-//                        .build())
-//                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-//                        .setSupported(true)
-//                        // Your server's client ID, not your Android client ID.
-//                        .setServerClientId(getString(R.string.default_web_client_id))
-//                        // Only show accounts previously used to sign in.
-//                        .setFilterByAuthorizedAccounts(true)
-//                        .build())
-//                // Automatically sign in when exactly one credential is retrieved.
-//                .setAutoSelectEnabled(true)
-//                .build();
-
         googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
         addEvents();
     }
     private void addEvents(){
-//        ActivityResultLauncher<IntentSenderRequest> activityResultLauncher
-//                = registerForActivityResult(new ActivityResultContracts.StartIntentSenderForResult(),
-//                new ActivityResultCallback<ActivityResult>() {
-//                    @Override
-//                    public void onActivityResult(ActivityResult o) {
-//                        if(o.getResultCode() == Activity.RESULT_OK){
-//                            try {
-//                                SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(o.getData());
-//                                String idToken = credential.getGoogleIdToken();
-//                                String username = credential.getId();
-//                                String password = credential.getPassword();
-//                                if (idToken !=  null) {
-//                                    String email = credential.getId();
-//                                    Toast.makeText(Login.this, email, Toast.LENGTH_LONG).show();
-//                                }
-//                            } catch (ApiException e) {
-//                                e.printStackTrace();
-//                            }
-//                        }
-//                    }
-//                });
-//        activityLoginBinding.buttonLogin.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                oneTapClient.beginSignIn(signUpRequest)
-//                        .addOnSuccessListener(Login.this, new OnSuccessListener<BeginSignInResult>() {
-//                            @Override
-//                            public void onSuccess(BeginSignInResult result) {
-//                                IntentSenderRequest intentSenderRequest
-//                                        = new IntentSenderRequest.Builder(result.getPendingIntent().getIntentSender()).build();
-//                                activityResultLauncher.launch(intentSenderRequest);
-//                            }
-//                        })
-//                        .addOnFailureListener(Login.this, new OnFailureListener() {
-//                            @Override
-//                            public void onFailure(@NonNull Exception e) {
-//                                // No saved credentials found. Launch the One Tap sign-up flow, or
-//                                // do nothing and continue presenting the signed-out UI.
-//                                Log.e("TAG", "Error occurred during sign-in process", e);
-//                            }
-//                        });
-//            }
-//        });
+
         activityLoginBinding.buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
